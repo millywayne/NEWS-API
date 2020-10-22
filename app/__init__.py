2d603301d4efa4ae0f4cd890aacf27f0afac5999
millywayne <achiengmillicent35@gmail.com>
2020-10-22 03:32:59 +0300

    created view function for the search route

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
-from app.config import DevConfig
+from .config import DevConfig
 from flask_bootstrap import Bootstrap
-
 
 # Initializing application
 app = Flask(__name__,instance_relative_config = True)
@@ -11,8 +10,16 @@ app = Flask(__name__,instance_relative_config = True)
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
 
-class Config:
-    '''
-    General configuration parent class
-    '''
-    NEWS_API_BASE_URL = ' http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=487c722040f046eaa31635a68e73583c'
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
+
+
+
+from app import views
+from app import error
+
+# class Config:
+#     '''
+#     General configuration parent class
+#     '''
+#     NEWS_API_BASE_URL = ' http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=487c722040f046eaa31635a68e73583c'
