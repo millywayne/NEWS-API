ef93452579b35b1d3c29523daad999d19d73dd05
millywayne <achiengmillicent35@gmail.com>
2020-10-28 09:03:09 +0300

    modified

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from config import DevConfig
 from flask_bootstrap import Bootstrap
 from app import main as main_blueprint
 
-
 bootstrap = Bootstrap()
 
 def create_app(config_name, config_options):
@@ -26,7 +25,9 @@ app.config.from_pyfile('config.py')
 bootstrap.init_app(app)
 
  # Registering the blueprint
-app.register_blueprint(main_blueprint,)
+from .main import main as main_blueprint
+
+app.register_blueprint(main_blueprint)
 
  # setting config
 from .requests import configure_request
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-export MOVIE_API_KEY='<9e192f3a77ad08ae719494f95bbe5be5>'
+export NEWS_API_KEY='<9e192f3a77ad08ae719494f95bbe5be5>'
 export SECRET_KEY='<c1045b64982bb8f816c1d9696e0b2a3b166c4f9b>'
